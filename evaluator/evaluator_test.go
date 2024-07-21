daa948e13fe177b177bc5490f4e90d450570f864
Vilius Andraitis <vilius.andraitis@gmail.com>
2024-07-21 17:46:39 +0300

    add evaluation of math infix expressions

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -9,13 +9,24 @@ import (
 
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
-		input string
+		input    string
 		expected int64
 	}{
 		{"5", 5},
 		{"10", 10},
 		{"-5", -5},
-        {"-10", -10},
+		{"-10", -10},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50 + 100 + -50", 0},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"20 + 2 * -10", 0},
+		{"50 / 2 * 2 + 10", 60},
+		{"2 * (5 + 10)", 30},
+		{"3 * 3 * 3 + 10", 37},
+		{"3 * (3 * 3) + 10", 37},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 
 	for _, tt := range tests {
@@ -27,7 +38,7 @@ func TestEvalIntegerExpression(t *testing.T) {
 
 func TestEvalBooleanExpression(t *testing.T) {
 	tests := []struct {
-		input string
+		input    string
 		expected bool
 	}{
 		{"true", true},
@@ -42,7 +53,7 @@ func TestEvalBooleanExpression(t *testing.T) {
 
 func TestBangOperator(t *testing.T) {
 	tests := []struct {
-		input string
+		input    string
 		expected bool
 	}{
 		{"!true", false},
