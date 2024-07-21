53af17f93f0fbc2b152cd886078ed738cc01f7d6
Vilius Andraitis <vilius.andraitis@gmail.com>
2024-07-21 18:29:27 +0300

    add implementation of boolean infix expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -72,6 +72,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 		case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 			return evalIntegerInfixExpression(operator, left, right)
+		case operator == "==":
+			return nativeBoolToBooleanObject(left == right)
+		case operator == "!=":
+			return nativeBoolToBooleanObject(left != right)
 		default:
 			return NULL
 	}
@@ -112,6 +116,28 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 			return &object.Integer{Value: leftVal * rightVal}
 		case "/":
 			return &object.Integer{Value: leftVal / rightVal}
+		case "<":
+			return nativeBoolToBooleanObject(leftVal < rightVal)
+		case ">":
+			return nativeBoolToBooleanObject(leftVal > rightVal)
+		case "==":
+			return nativeBoolToBooleanObject(leftVal == rightVal)
+		case "!=":
+			return nativeBoolToBooleanObject(leftVal != rightVal)
+		default:
+			return NULL
+	}
+}
+
+func evalBooleanInfixExpression(operator string, left, right object.Object) object.Object {
+	leftVal := left.(*object.Boolean).Value
+	rightVal := right.(*object.Boolean).Value
+
+	switch operator {
+		case "==":
+			return &object.Boolean{Value: leftVal == rightVal}
+		case "!=":
+			return &object.Boolean{Value: leftVal != rightVal}
 		default:
 			return NULL
 	}
